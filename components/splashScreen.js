50115764c38b1d6f930b248354ef80407560d389
Daniel Shumway <shumway.danny@gmail.com>
2014-07-11 10:37:05 -0400

    First commit of working engine.  We'll do some cleanup and make this easier to work with now.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

---
+++ components/splashScreen.js
@@ -0,0 +1,13 @@
+function Title(){
+	//------------------------------VARIABLES-------------------------------------
+	var base = Module(0, 0, 1920, 1080); //Call base
+	var toReturn = base.interface; //Set toReturn via base.
+	Touch.Collisions(base);
+
+	//Set up the title screen.
+
+
+	
+
+	return toReturn;
+}
