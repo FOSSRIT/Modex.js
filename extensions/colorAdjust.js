8f8ebe40ef919d8e1e7db2eef1ce5c69423e5a83
Daniel Shumway <shumway.danny@gmail.com>
2014-07-31 10:04:35 -0400

    Added colorAdjust function for sprites, still doing testing on it.  Doesn't work offline.

---
+++ extensions/colorAdjust.js
@@ -0,0 +1,62 @@
+var ColorAdjust = function(){
+	var toReturn = {};
+
+
+
+	//R, G, and B range from 0 to 255, Sprite is the sprite module you want to change.
+	toReturn.multiplyFilter = function(r, g, b, Sprite)
+	{
+		//Only works on sprites.
+		if(Sprite.type = "Sprite") {
+			//Get relevant data
+			var data = Sprite.getData();
+			if(data.width != 0 && data.height != 0) {
+
+				//Set up the virtual canvas
+				var filter = document.createElement("canvas");
+				filter.width = data.width;
+				filter.height = data.height;
+				var ctx = filter.getContext("2d");
+
+				//Draw on the virtual canvas.
+				ctx.drawImage(data.image, 0, 0);
+				var imgData = ctx.getImageData(0, 0, filter.width, filter.height);
+				var pix = imgData.data;
+
+
+				//If there is a previous filter, remove it
+				if(Sprite.lastFilter) {
+					pix[i] = pix[i]*255/Sprite.lastFilter[r];
+					pix[i+1] = pix[i+1]*255/Sprite.lastFilter[g];
+					pix[i+2] = pix[i+2]*255/Sprite.lastFilter[b];
+				}
+
+				//And set the new data.
+				Sprite.lastFilter = {"r":r, "g":g, "b":b };
+
+
+				//And apply the new filter.
+				for(var i = 0, n = pix.length; i < n; i += 4) {
+					pix[i] = r*pix[i]/255; //r
+					pix[i+1] = g*pix[i+1]/255//g
+					pix[i+2] = b*pix[i+2]/255//b
+					//alpha (ingore this)
+				}
+
+				ctx.putImageData(imgData, 0, 0);
+
+				//Convert back to image.
+				var img = new Image();
+				img.src = filter.toDataURL();
+				img.onload = function(){
+					Sprite.setImage(img, true);
+				}
+			}
+		}
+	}
+
+
+
+	return toReturn;
+
+}(); //Calls itself.
