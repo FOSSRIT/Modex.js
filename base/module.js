a6e38a59985f7cab9d59b3768a727fdf6251c1d3
Daniel Shumway <shumway.danny@gmail.com>
2014-08-03 17:13:36 -0400

    Fixed resulting bug where events weren't handling toFire correctly.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

--- base/module.js
+++ base/module.js
@@ -69,23 +69,29 @@ function Module(_x, _y, _width, _height){
 
 	function _handleEvent(_eventString, _clipBoard) {
 
-		//If the event exists.
-		if(toReturn.events[_eventString]) {
 
-			//Loop through the list of functions attached to that event and fire them off.
-			//Those functions can be anywhere, but they're mostly going to be internal.
-			//Before we run them, we want to update our internal clipboard so they have access to it.
-			for(var i=0; i<toReturn.events[_eventString].call.length; i++) {
+		//If the event isn't blocked.  And if you're not set to block all events.
+		if((!_clipBoard.BlockEvents || _clipBoard.BlockEvents.indexOf(_eventString) !=-1) && toReturn.interface.recieveEvents) {
 
-				//If the event isn't blocked.  And if you're not set to block all events.
-				if((!toReturn.clipBoard.BlockEvents || toReturn.clipBoard.BlockEvents.indexOf(_eventString) !=-1) && toReturn.interface.recieveEvents) {
-					//Call functions.
 
-					//FOR POSTERITY:  If you're utalizing this part of the code - ie, calling a custom function
-					//and then passing down events on your own, you should probably set bubble to false.
-					//You can handle passing the event down and managing the clipboard in all likelyhood - bubble is mostly for automation's sake.
-					toReturn.events[_eventString].call[i].call(this, _clipBoard);
+			//If the event exists.
+			if(toReturn.events[_eventString]) {
 
+				//Loop through the list of functions attached to that event and fire them off.
+				//Those functions can be anywhere, but they're mostly going to be internal.
+				//Before we run them, we want to update our internal clipboard so they have access to it.
+				for(var i=0; i<toReturn.events[_eventString].call.length; i++) {
+
+					//If the event isn't blocked.  And if you're not set to block all events.
+					if((!_clipBoard.BlockEvents || _clipBoard.BlockEvents.indexOf(_eventString) !=-1) && toReturn.interface.recieveEvents) {
+						//Call functions.
+
+						//FOR POSTERITY:  If you're utalizing this part of the code - ie, calling a custom function
+						//and then passing down events on your own, you should probably set bubble to false.
+						//You can handle passing the event down and managing the clipboard in all likelyhood - bubble is mostly for automation's sake.
+						toReturn.events[_eventString].call[i].call(this, _clipBoard);
+
+					}
 				}
 			}
 
@@ -97,6 +103,7 @@ function Module(_x, _y, _width, _height){
 				}
 			}
 
+
 			//Bubbling events.
 			if( //This might be able to be shortened.
 				//If we're set to auto bubble and either the event doesn't exist or is set to bubble.
@@ -105,6 +112,7 @@ function Module(_x, _y, _width, _height){
 				//If the event exists and is set to bubble downward.
 				(toReturn.events[_eventString] && toReturn.events[_eventString].bubble)) 
 			{
+
 
 				//Pass the event on.
 				var fireList = _clipBoard.ToFire || [];
@@ -158,7 +166,7 @@ function Module(_x, _y, _width, _height){
 						if(toReturn.contents[j] != _returnFrom){ //Not the same module.
 
 							//Fire off if not blocked.
-							if(!toReturn.clipBoard.BlockEvents || toReturn.clipBoard.BlockEvents.indexOf(_toFire[i]) != -1){
+							if(!_clipBoard.BlockEvents || _clipBoard.BlockEvents.indexOf(_toFire[i]) != -1){
 								toReturn.contents[j].handleEvent(_toFire[i], _clipBoard);
 							}
 						}
